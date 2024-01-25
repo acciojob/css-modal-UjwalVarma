b8f611c30c5133ca6169565776cc69ddf2aeff50
UjwalVarma <120555973+UjwalVarma@users.noreply.github.com>
2024-01-25 21:33:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// script.js
+
+document.getElementById("openModal").addEventListener("click", function() {
+  document.getElementById("myModal").style.display = "block";
+});
+
+document.getElementById("closeModal").addEventListener("click", function() {
+  document.getElementById("myModal").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
